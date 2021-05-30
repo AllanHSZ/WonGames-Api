98aa0594e8d5261daabd77b0656e0de20293e7ed
Allan <allanhsz@outlook.com>
2021-05-30 20:55:29 -0300

    Iniciando o projeto

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdb4a10344eebdfcb1f164e470f51994'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f16e56aa-5c98-49af-8146-3a049e10967a'
+};
